73ce2af0968c2a66ba176fad849765f0761f7392
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-02-01 16:50:55 +0600

    frontend banner carousel section finished

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,55 @@
+import Carousel from "react-multi-carousel";
+import {Link} from "react-router-dom";
+import "react-multi-carousel/lib/styles.css";
+
+const Banner = () => {
+    
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+            // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            // optional, default to 1.
+        }
+    };
+    
+    return (
+        <div className="w-full ">
+            <div className="customContainer">
+                <div className="w-full flex flex-wrap md-lg:gap-8">
+                    <div className="w-full">
+                        <div className="my-8">
+                            <Carousel responsive={responsive}
+                                      autoPlay={true}
+                                      infinite={true}
+                                      arrows={true}
+                                      showDots={true}
+                                      dotListClass={"custom-dot-list-style"}
+                                      draggable={false}
+                                      keyBoardControl={true}
+                                      transitionDuration={500}>
+                                {
+                                    [1,2,3,4,5,6,7].map((item,index)=> (
+                                        <Link to={"/"} key={index} className="md-lg:h-[440pc] h-auto w-full">
+                                            <img src={`http://localhost:5173/images/banner/${item}.jpg`} className="w-full h-full object-contain" alt=""/>
+                                        </Link>
+                                    ))
+                                }
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Banner
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Headers from "../components/Headers"
+import Banner from "../components/Banner.jsx";
 
 const Home = () => {
     return (
         <div className="w-full">
             <Headers/>
+            <Banner/>
         </div>
     )
 }
