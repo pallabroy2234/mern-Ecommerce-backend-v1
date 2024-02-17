926561c9dff13df9366528b989668e12e47d3854
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-02-17 21:42:55 +0600

    Add: add to card section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Shop from "./pages/Shop.jsx";
+import Card from "./pages/Card.jsx";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/shop" element={<Shop/>}/>
+                <Route path="/card" element={<Card/>}/>
             </Routes>
         </BrowserRouter>
     )
