bf4a442bd6bf39c540b70e4065a2db10f13919ff
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-02-19 15:09:09 +0600

    Add: User review section

--- frontend/src/components/Review.jsx
+++ frontend/src/components/Review.jsx
@@ -4,6 +4,7 @@ import RattingTemp from "./RattingTemp.jsx";
 const Review = () => {
     return (
         <div className="mt-8">
+            {/* Ratting */}
             <div className="flex gap-10 md:flex-col">
                 <div className="flex flex-col gap-2 justify-start items-start py-4">
                     <div>
@@ -79,8 +80,31 @@ const Review = () => {
                         <p className="text-sm text-slate-600 w-[0%]">0</p>
                     </div>
                 </div>
+            </div>
+            
+            <h2 className="text-slate-600 text-xl font-bold py-5">Product Reviews 30</h2>
+            
+            <div className="flex flex-col gap-8 pb-10 pt-4">
+                {
+                    [1,2,3,4,5,6].map((item,index)=> (
+                        <div key={index} className="flex flex-col gap-1">
+                            <div className="flex justify-between items-center">
+                                <div className="flex gap-1 text-xl">
+                                    <RattingTemp ratting={4}/>
+                                </div>
+                                <span className="text-slate-600">19 Feb 2024</span>
+                            </div>
+                            <span className="text-slate-600 text-lg font-semibold">Pallab Roy Tushar</span>
+                            <p className="text-base text-slate-600">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque autem deleniti ea exercitationem illo iste, iure laudantium officiis pariatur, quis quod ratione recusandae reiciendis sint veritatis? Cupiditate fuga illum quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolorum itaque possimus sed. Eaque eligendi exercitationem itaque iure, labore, magni obcaecati odit pariatur perspiciatis praesentium quae quia, quod vitae? Officiis?
+                            </p>
+                        </div>
+                    ))
+                }
+            
             
             </div>
+        
         </div>
     )
 }
