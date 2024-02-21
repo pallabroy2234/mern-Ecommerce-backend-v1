13fb283b9ade4b5c0fe77c37d084db6e64dc1bea
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-02-21 15:02:05 +0600

    React Host Toast setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,23 @@ import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux";
 import store from "./store/index.js";
+import {Toaster} from "react-hot-toast";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <Provider store={store}>
-        <App />
-    </Provider>
-  </>,
+    <>
+        <Provider store={store}>
+            <App/>
+            <Toaster
+                toastOptions={{
+                    position: "top-right",
+                    style: {
+                        background: "white",
+                        color: "black",
+                        animationDuration: "0.5s",
+                    }
+                }}
+            />
+        </Provider>
+    </>,
 )
