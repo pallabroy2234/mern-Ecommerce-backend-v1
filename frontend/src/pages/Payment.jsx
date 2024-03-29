5266f7fb1a74699d643da41e36a3838d3051d5e5
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-03-29 23:39:18 +0600

    payment page almost ready

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -8,7 +8,9 @@ import Stripe from "../components/Stripe.jsx";
 const Payment = () => {
 	const location = useLocation();
 	const [paymentMethod, setPaymentMethod] = useState("stripe");
-	console.log(location);
+	const {
+		state: {items, price, userId, orderId},
+	} = location;
 
 	return (
 		<div>
@@ -16,8 +18,9 @@ const Payment = () => {
 			<section className='bg-[#eeeeee]'>
 				<div className='customContainer py-16 mt-4'>
 					<div className='flex flex-wrap md:flex-col-reverse'>
+						{/* left section */}
 						<div className='w-7/12 md:w-full'>
-							<div className='pr-2 md:pr-2'>
+							<div className='pr-2 md:pr-0'>
 								<div className='flex flex-wrap justify-between'>
 									{/* Stripe */}
 									<div onClick={() => setPaymentMethod("stripe")} className={`w-[20%] border border-r cursor-pointer py-8 px-12 ${paymentMethod === "stripe" ? "bg-white" : "bg-slate-100"}`}>
@@ -47,12 +50,12 @@ const Payment = () => {
 										</div>
 									</div>
 									{/* Roket */}
-									<div onClick={() => setPaymentMethod("roket")} className={`w-[20%] border border-r cursor-pointer py-8 px-12 ${paymentMethod === "roket" ? "bg-white" : "bg-slate-100"}`}>
+									<div onClick={() => setPaymentMethod("rocket")} className={`w-[20%] border border-r cursor-pointer py-8 px-12 ${paymentMethod === "rocket" ? "bg-white" : "bg-slate-100"}`}>
 										<div className='flex flex-col gap-[3px] justify-center items-center'>
 											<div className=''>
-												<img src={"images/payment/roket.png"} className='w-full h-full object-cover' alt={"roket"} />
+												<img src={"images/payment/rocket.png"} className='w-full h-full object-cover' alt={"rocket"} />
 											</div>
-											<span className='text-slate-600'>Roket</span>
+											<span className='text-slate-600'>Rocket</span>
 										</div>
 									</div>
 								</div>
@@ -71,6 +74,38 @@ const Payment = () => {
 										</div>
 									</div>
 								)}
+
+								{paymentMethod === "nogod" && (
+									<div className='mt-3'>
+										<div className='w-full px-4 py-8 bg-white shadow-sm'>
+											<button className='px-10 py-[6px] rounded-sm hover:shadow-orange-500/20 bg-orange-500 text-white'>Pay Now</button>
+										</div>
+									</div>
+								)}
+								{paymentMethod === "rocket" && (
+									<div className='mt-3'>
+										<div className='w-full px-4 py-8 bg-white shadow-sm'>
+											<button className='px-10 py-[6px] rounded-sm hover:shadow-orange-500/20 bg-orange-500 text-white'>Pay Now</button>
+										</div>
+									</div>
+								)}
+							</div>
+						</div>
+
+						{/* right section */}
+						<div className='w-5/12 md:w-full'>
+							<div className='pl-2 md:pl-0 md:mb-0'>
+								<div className='bg-white shadow p-5 text-slate-600 flex flex-col gap-3'>
+									<h2>Order Summery</h2>
+									<div className='flex justify-between items-center gap-2'>
+										<span>{items || ""} items and shipping fee included</span>
+										<span>{price || ""}</span>
+									</div>
+									<div className='flex justify-between items-center gap-2 font-semibold'>
+										<span>Total Amount</span>
+										<span className='text-lg text-orange-500'>{price || ""}</span>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
