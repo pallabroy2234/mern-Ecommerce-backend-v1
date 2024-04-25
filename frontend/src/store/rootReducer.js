26f6971222a1700a660bda9ded0dc2f68362cbb9
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-04-25 15:59:55 +0600

    frontend: delete payment reducer

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -4,7 +4,6 @@ import cartReducer from "./reducers/cartReducer.js";
 import orderReducer from "./reducers/orderReducer.js";
 import dashboardReducer from "./reducers/dashboardReducer.js";
 import chatReducer from "./reducers/chatReducer.js";
-import paymentReducer from "./reducers/paymentReducer.js";
 
 const rootReducer = {
 	home: homeReducer,
@@ -13,7 +12,6 @@ const rootReducer = {
 	order: orderReducer,
 	dashboard: dashboardReducer,
 	chat: chatReducer,
-	payment: paymentReducer,
 };
 
 export default rootReducer;
