61d781c93feba2cf03552e8c601a25a5956948b5
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-05 01:51:15 +0600

    Remove backend folder

--- backend/src/routes/frontend/bannerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {handleGetBanner} = require("../../controllers/frontend/bannerControllers");
-const bannerRouter = express.Router();
-
-// * GET BANNER RANDOMLY -> 10 || GET || /api/frontend/banner/get-banner
-bannerRouter.get("/get-banner", handleGetBanner);
-
-module.exports = bannerRouter;
