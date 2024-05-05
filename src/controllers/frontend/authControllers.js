5b578c4dc26210fc1fded97bbd1d8eb389d2f773
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-06 01:26:16 +0600

    same site strict

--- src/controllers/frontend/authControllers.js
+++ src/controllers/frontend/authControllers.js
@@ -86,7 +86,7 @@ const handleUserLogin = async (req, res) => {
 			expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
 			httpOnly: true,
 			secure: true,
-			sameSite: "none",
+			sameSite: "strict",
 		});
 
 		return successResponse(res, {
