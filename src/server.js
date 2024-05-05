1599b2776b1962c3f7a8c8b46bc713b9a842ab1e
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-05 18:07:32 +0600

    some refactor

--- src/server.js
+++ src/server.js
@@ -10,15 +10,7 @@ const server = http.createServer(app);
 
 
 
-app.use(
-	cors({
-		// origin: /.*/,
-		origin: process.env.MODE === "production" ? [process.env.CLIENT_USER_PRODUCTION_URL, process.env.CLIENT_DASHBOARD_PRODUCTION_URL] :
-			[process.env.CLIENT_USER_LOCAL_URL, process.env.CLIENT_DASHBOARD_LOCAL_URL],
-		preflightContinue: false,
-		credentials: true,
-	}),
-);
+
 
 
 const io = socketIo(server, {
