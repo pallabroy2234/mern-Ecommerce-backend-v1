1771fae951b2fbecb1ae629deecf8537414f512b
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-06 01:19:42 +0600

    cros config

--- src/app.js
+++ src/app.js
@@ -35,18 +35,19 @@ if (process.env.NODE_ENV === 'development') {
 
 // Use CORS middleware with dynamic allowed origins
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            let msg = "The CORS policy for this site does not allow access from the specified Origin.";
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    // origin: function (origin, callback) {
+    //     // allow requests with no origin (like mobile apps or curl requests)
+    //     if (!origin) return callback(null, true);
+    //     if (allowedOrigins.indexOf(origin) === -1) {
+    //         let msg = "The CORS policy for this site does not allow access from the specified Origin.";
+    //         return callback(new Error(msg), false);
+    //     }
+    //     return callback(null, true);
+    // },
+    origin: /.*/,
     credentials: true,
     optionsSuccessStatus: 200,
-    // preflightContinue: false,
+    preflightContinue: false,
 }));
 
 
